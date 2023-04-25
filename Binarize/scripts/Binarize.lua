bd42fc4134413721a05bd1c62f9bea51ea52f12e
GitLab Runner <gitlab-runner@ip-172-31-13-207.eu-central-1.compute.internal>
2023-04-25 18:27:59 +0000

    Update

--- Binarize/scripts/Binarize.lua
+++ Binarize/scripts/Binarize.lua
@@ -27,7 +27,7 @@ print('AppEngine Version: ' .. Engine.getVersion())
 local DELAY = 1500 -- ms between each type for demonstration purpose
 
 -- Creating viewer
-local viewer = View.create("viewer2D1")
+local viewer = View.create()
 
 --End of Global Scope-----------------------------------------------------------
 
